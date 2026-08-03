fc3b0447dea689e8145e4bb50153682f3b67dd07
cyberhatc <userattest000013@gmail.com>
2026-08-03 13:15:50 +0530

    Update banner to neofetch-style layout with ASCII art logo and system info side by side

--- banner.sh
+++ banner.sh
@@ -1,17 +1,65 @@
 #!/bin/bash
 
 T='\e[36m'   # teal
+G='\e[32m'   # green for logo
 R='\e[0m'    # reset
 
-cat <<EOF
-${T} cyberhatc@devil_Mike${R} ${T}|${R} ${T}|${R}
-${T} --------------- ___${R}
-${T} | |___${R}              OS: HackOS 6.8.0-cyber x86_64
-${T} / _ \\\\| / __|${R}        Host: devil_Mike Workstation
-${T} __/| \\\\__ \\\\${R}       Kernel: 6.8.0-cyber
-${T} \\\\___||_|___/${R}       Uptime: since age 14
-${T} Shell${R}: /bin/zsh 5.9
-${T} WM${R}: i3-gaps
-${T} CPU${R}: devil_Mike @ 5.1GHz
-${T} Memory${R}: 5x hackathon runner-up
-EOF
+# ASCII art logo - stylized skull/hacker logo
+logo=(
+"${G}         .-/+oossssoo+/-.        ${R}"
+"${G}     \`:+ssssssssssssssssss+:\`     ${R}"
+"${G}   .+ssssssssssssssssssssssss+.    ${R}"
+"${G}  +sssssssssssssssssssssssssss+   ${R}"
+"${G} +ssssssssssssssssssssssssssss+  ${R}"
+"${G} +ssssssssssssssssssssssssssss+  ${R}"
+"${G} .ssssssssssssssssssssssssssss.  ${R}"
+"${G}  hNNNNNNNNNNNNNNNNNNNNNNNNh    ${R}"
+"${G}  hNNNNMMNNNNNNNNNNNNNNNNNh    ${R}"
+"${G}  hNNNNMMNNNNNNNNNNNNNNNNh     ${R}"
+"${G}  hNNNNNNNNNNNNNNNNNNNNNNh     ${R}"
+"${G}  hNNNNMMNNNNNNNNNNNNNNNNh     ${R}"
+"${G}  hNNNNMMNNNNNNNNNNNNNNNNNh    ${R}"
+"${G}  hNNNNNNNNNNNNNNNNNNNNNNNNh   ${R}"
+"${G}  \`+ssssssssssssssssssssss+    ${R}"
+"${G}   .ssssssssssssssssssssss.     ${R}"
+"${G}    +sssssssssssssssssss+       ${R}"
+"${G}     \`:+ssssssssssssss+:\`       ${R}"
+"${G}         .-/+oossssoo+/-.       ${R}"
+)
+
+# System info lines
+info=(
+"${T}OS${R}: HackOS 6.8.0-cyber x86_64"
+"${T}Host${R}: devil_Mike Workstation"
+"${T}Kernel${R}: 6.8.0-cyber"
+"${T}Uptime${R}: since age 14"
+"${T}Shell${R}: /bin/zsh 5.9"
+"${T}WM${R}: i3-gaps"
+"${T}CPU${R}: devil_Mike @ 5.1GHz"
+"${T}Memory${R}: 5x hackathon runner-up"
+)
+
+# Print header
+echo ""
+printf "${T}cyberhatc@devil_Mike${R}\n"
+printf "${T}--------------------${R}\n"
+
+# Print logo and info side by side
+for i in "${!logo[@]}"; do
+    printf "  ${logo[$i]}  "
+    if [ $i -lt ${#info[@]} ]; then
+        printf "  ${info[$i]}"
+    fi
+    echo ""
+done
+
+# Print remaining info lines if logo is longer
+if [ ${#info[@]} -gt ${#logo[@]} ]; then
+    for ((i=${#logo[@]}; i<${#info[@]}; i++)); do
+        printf "                          "
+        printf "  ${info[$i]}"
+        echo ""
+    done
+fi
+
+echo ""
