ec3524b31513884dad937fa9b7a716686c5dba1e
cyberhatc <userattest000013@gmail.com>
2026-08-03 13:03:47 +0530

    Add teal terminal banner script

---
+++ banner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+T='\e[36m'   # teal
+R='\e[0m'    # reset
+
+cat <<EOF
+${T} cyberhatc@devil_Mike${R} ${T}|${R} ${T}|${R}
+${T} --------------- ___${R}
+${T} | |___${R}              OS: HackOS 6.8.0-cyber x86_64
+${T} / _ \\\\| / __|${R}        Host: devil_Mike Workstation
+${T} __/| \\\\__ \\\\${R}       Kernel: 6.8.0-cyber
+${T} \\\\___||_|___/${R}       Uptime: since age 14
+${T} Shell${R}: /bin/zsh 5.9
+${T} WM${R}: i3-gaps
+${T} CPU${R}: devil_Mike @ 5.1GHz
+${T} Memory${R}: 5x hackathon runner-up
+EOF
